bacd173b01ae1dcd25dc0bafb0baba8b9ffdac0a
Gilson Zurc <51736921+GilsonZurc@users.noreply.github.com>
2026-03-09 12:13:11 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+import nextPWA from "next-pwa";
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    appDir: true,
+    appDir: true, // Nota: Se você estiver usando o Next.js 13.4 ou superior, o appDir já é o padrão e essa linha pode ser removida.
   },
 };
 
-const withPWA = require("next-pwa")({
+const withPWA = nextPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
